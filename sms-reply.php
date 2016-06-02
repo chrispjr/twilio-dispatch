afc9451bbe408ab1d7a255552994e3533a703b61
Chris Pittman Jr <chrispittmanjr@gmail.com>
2016-06-01 23:57:30 -0400

    add smartstreets api response

--- sms-reply.php
+++ sms-reply.php
@@ -3,20 +3,21 @@
 $body = $_REQUEST['Body'];
 
 if ($body == "pickup") {
-	header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-    ?>
-	<Response>
-	    <Message>What's your address?</Message>
-	</Response>
+		header("content-type: text/xml");
+	    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	    ?>
+		<Response>
+		    <Message>What's your address?</Message>
+		</Response>
+    <?php } elseif($body == "debug") {
+		header("content-type: text/xml");
+	    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	    ?>
+	    <Response>
+	        <Message><?php var_dump($_REQUEST); ?></Message>
+	    </Response>
     <?php } else {
-	header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-    ?>
-    <Response>
-        <Message><?php var_dump($_REQUEST); ?></Message>
-    </Response>
-    <?php
-}
+    	curl -v 'https://api.smartystreets.com/street-address?auth-id=f0630cb2-1a6e-a4c9-df41-2d1192123666&auth-token=eRQJMxOomkO0ksHbLPNS&street='.$body.'&candidates=10';
+    }
 
 ?>
