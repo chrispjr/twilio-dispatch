d7218eebe17aa64baf064f878a06a8ead87ed352
Chris Pittman Jr <chrispittmanjr@gmail.com>
2016-06-02 00:38:43 -0400

    add ss debug

---
+++ ss.php
@@ -0,0 +1,23 @@
+<?php
+
+$body = $_REQUEST['Body'];
+
+$url = "https://api.smartystreets.com/street-address";
+$ss['auth_id'] = "f0630cb2-1a6e-a4c9-df41-2d1192123666";
+$ss['auth_token'] = "eRQJMxOomkO0ksHbLPNS";
+$ss['street'] = urlencode($body);
+$ss['candidates'] = "10";
+$ss_query = http_build_query($ss);
+$ch = curl_init();
+$ss_options = array(
+	CURLOPT_URL => $url,
+	CURLOPT_POST => false,
+	CURLOPT_POSTFIELDS => $ss_query,
+	CURLOPT_RETURNTRANSFER => true
+);
+curl_setopt_array($ch, $ss_options);
+$ss_results = curl_exec($ch);
+curl_close($ch);
+var_dump($ss_results); 
+
+?>
