ba8d292f8669a0cc5f299664798c37f275e02fca
Chris Pittman Jr <chrispittmanjr@gmail.com>
2016-06-01 23:44:04 -0400

    reply with vardump

--- sms-reply.php
+++ sms-reply.php
@@ -1,13 +1,7 @@
 <?php
-
-var_dump($_REQUEST);
-
-?>
-
-<?php
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Hello, Mobile Monkey</Message>
+    <Message><?php var_dump($_REQUEST); ?></Message>
 </Response>
