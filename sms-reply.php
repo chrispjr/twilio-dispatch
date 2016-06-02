ed45295b56ea65b89a99ff0fc932df4d44940fb9
Chris Pittman Jr <chrispittmanjr@gmail.com>
2016-06-01 23:51:06 -0400

    variable response

--- sms-reply.php
+++ sms-reply.php
@@ -1,7 +1,22 @@
 <?php
-    header("content-type: text/xml");
+
+$body = $_REQUEST['body'];
+
+if ($body == "pickup") {
+	header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    ?>
+	<Response>
+	    <Message>What's your address?</Message>
+	</Response>
+    <?php } else {
+	header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    ?>
+    <Response>
+        <Message><?php var_dump($_REQUEST); ?></Message>
+    </Response>
+    <?php
+}
+
 ?>
-<Response>
-    <Message><?php var_dump($_REQUEST); ?></Message>
-</Response>
