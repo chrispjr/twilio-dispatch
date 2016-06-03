a50268eec337b8736a5b7a29b83106f15577b699
Chris Pittman Jr <chrispittmanjr@gmail.com>
2016-06-03 02:24:58 -0400

    var_dump ss_results

--- sms-reply.php
+++ sms-reply.php
@@ -22,7 +22,7 @@ function send_validation_response($ss_results) {
 
 		foreach ($ss_results as $ss_result => $ss_address_object) {
 
-			var_dump($ss_result);
+			var_dump($ss_address_object);
 			
 			// var_dump($ss_address_object->delivery_line_1);
 
