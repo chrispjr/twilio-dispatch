2fad50495600f85eef01ea2714d4abcdddcd2eaa
Chris Pittman Jr <chrispittmanjr@gmail.com>
2016-06-03 18:42:20 -0400

    create ss_validate_address function

--- sms-reply.php
+++ sms-reply.php
@@ -23,7 +23,7 @@ function send_validation_response($ss_results) {
 		foreach ($ss_results as $ss_result => $ss_address_object) {
 
 			var_dump($ss_address_object);
-			var_dump("---------")
+			var_dump("---------");
 
 
 			
