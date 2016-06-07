2f33fe2d6b38cae9e6cac3da1b802ad3c3aaf6da
Chris Pittman Jr <chrispittmanjr@gmail.com>
2016-06-06 23:22:22 -0400

    set TwiMLResponse

--- sms-reply-2.php
+++ sms-reply-2.php
@@ -146,7 +146,7 @@ header("content-type: text/xml");
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
-$TwiMLResponse = pickup_conversation();
+$TwiMLResponse = "pickup_conversation()";
 
 ?>
 <Response>
